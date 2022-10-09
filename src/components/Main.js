9161a74f4b1189714d0cd88da779f9a23d857648
Kingnels <ifeanyiibe2022@gmail.com>
2022-10-09 14:04:25 +0100

    new commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
 function Main() {
+  const handle = function(){
+    console.log("i was clicked")
+  }
   return (
     <div className='main-section'>
-        <form action="" className='main-form'>
+        <div action="" className='main-form'>
             <input type="text" className='meme-input' placeholder='top text'/>
             <input type="text" className='meme-input' placeholder='lower text'/>
-            <button className='meme-btn'>Get a new meme Image</button>
-        </form>
+            <button 
+            className='meme-btn' 
+            onClick = {handle}
+            // onMouseEnter= {handle}
+            >
+              Get a new meme Image
+            </button>
+        </div>
     </div>
   )
 }
