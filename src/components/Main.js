ffd52ab1cc20cf977ad0c23248f0ba273437ff42
Kingnels <ifeanyiibe2022@gmail.com>
2022-10-10 19:13:07 +0100

    new commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,20 @@ import React, {useState} from 'react'
 import Meme from "./memedata"
 
 function Main() {
-    const [memeImage, setMemeImage] = useState("https://i.imgflip.com/1yxkcp.jpg")
+    const [memeImage, setMemeImage] = useState("https://i.imgflip.com/1ihzfe.jpg")
     // const [memeImage, setMemeImage] = React.useState("")
     function handle (){
     const memeArray = Meme.data.memes
     const randNum = Math.floor(Math.random()* memeArray.length)
     const url = memeArray[randNum].url
     // const {url}= memeArray[randNum]
-    setMemeImage(url)
-    console.log(url)
+    setMemeImage(() => {
+      if(url === 0){
+      return url + 1
+      }else{
+        return url
+      }
+    })
   }
 
   return (
@@ -22,10 +27,10 @@ function Main() {
             className='meme-btn' 
             onClick = {handle}
             >
-              Get a new meme Image
+              Get a new meme Image 🖼
             </button>
         </div>
-        
+
         <img className='meme-img' src={memeImage} alt="" />
     </div>
   )
