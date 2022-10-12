c4233655cf1fc2ee8ad8e24019d948656c8e88a8
Kingnels <ifeanyiibe2022@gmail.com>
2022-10-12 10:29:15 +0100

    new

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,19 +2,27 @@ import React, {useState} from 'react'
 import Meme from "./memedata"
 
 function Main() {
-    const [memeImage, setMemeImage] = useState("https://i.imgflip.com/1ihzfe.jpg")
+    // const [memeImage, setMemeImage] = useState("https://i.imgflip.com/1ihzfe.jpg")
+    const [joke, setJoke] = useState({
+      topText:"",
+      botttomText:"",
+      randomImage: "https://i.imgflip.com/1ihzfe.jpg"
+    })
+
+    const [allData, setAllData] = useState(Meme)
+
     // const [memeImage, setMemeImage] = React.useState("")
     function handle (){
-    const memeArray = Meme.data.memes
-    const randNum = Math.floor(Math.random()* memeArray.length)
-    const url = memeArray[randNum].url
-    // const {url}= memeArray[randNum]
+    const memeArray = allData.data.memes
+    const randNum = Math.floor(Math.random()* memeArray.length) !== 0 ? Math.floor(Math.random()* memeArray.length) : Math.floor(Math.random()* memeArray.length) + 1;
+    const url = memeArray[randNum].url    
 
-            setMemeImage(() => {
-              if(url === 0){
-              return url + 1
-              }else{
-                return url
+    // const {url}= memeArray[randNum]
+    
+            setJoke((prevState) => {
+              return {
+                ...prevState,
+                randomImage : url
               }
             })
           }
@@ -31,7 +39,8 @@ function Main() {
               Get a new meme Image 🖼
             </button>
         </div>
-        <img className='meme-img' src={memeImage} alt="" />
+
+        <img className='meme-img' src={joke.randomImage} alt="" />
     </div>
   )
 }
