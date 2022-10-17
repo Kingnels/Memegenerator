5378b50b32ad0b6c53e893c3daa6367d87cb77a7
Kingnels <ifeanyiibe2022@gmail.com>
2022-10-17 20:10:54 +0100

    text funcitonality added

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,6 +13,7 @@ function Main() {
     const [allData, setAllData] = useState(Meme)
 
     // const [memeImage, setMemeImage] = React.useState("")
+    
     function handle (){
     const memeArray = allData.data.memes
     const randNum = Math.floor(Math.random()* memeArray.length) !== 0 ? Math.floor(Math.random()* memeArray.length) : Math.floor(Math.random()* memeArray.length) + 1;
@@ -27,11 +28,19 @@ function Main() {
             })
           }
 
+    function handleChange(event){
+      const {name, value} = event.target
+      setJoke(prevState =>({
+          ...prevState,
+          [name]:value
+      }))
+    }
+
   return (
     <div className='main-section'>
         <div action="" className='main-form'>
-            <input type="text" className='meme-input' placeholder='top text'/>
-            <input type="text" className='meme-input' placeholder='lower text'/>
+            <input type="text" className='meme-input' placeholder='top text' name='topText' value={joke.topText} onChange={handleChange}/>
+            <input type="text" className='meme-input' placeholder='lower text' name="botttomText" value={joke.botttomText} onChange={handleChange}/>
             <button 
             className='meme-btn' 
             onClick = {handle}
@@ -40,7 +49,16 @@ function Main() {
             </button>
         </div>
 
-        <img className='meme-img' src={joke.randomImage} alt="" />
+        <div className="meme-text">
+                <img className='meme-img' src={joke.randomImage} alt="" />
+                <h2 className='top text'>{joke.topText}</h2>
+                <h2 className='bom text'>{joke.botttomText}</h2>
+                {/* <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2> */}
+            </div>
+
+          
+         
     </div>
   )
 }
